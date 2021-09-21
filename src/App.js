bbaba05c44577b99708b20241b7e9fe86da36b5d
Josue Lobo <darksiders20005@gmail.com>
2021-09-21 16:42:28 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Comidas from './pages/Comidas';
-import Bebidas from './pages/Bebidas';
-import DetalhesComida from './pages/DetalhesComida';
-import DetalhesBebida from './pages/DetalhesBebida';
-import ComidaEmProcesso from './pages/ComidaEmProcesso';
-import BebidaEmProgresso from './pages/BebidaEmProgresso';
-import Explorar from './pages/Explorar';
-import ExplorarComidas from './pages/ExplorarComidas';
-import ExplorarBebidas from './pages/ExplorarBebidas';
-import ComidasIngredientes from './pages/ComidasIngredientes';
-import BebidasIngredientes from './pages/BebidasIngredientes';
-import ComidasOrigem from './pages/ComidaOrigem';
-import Perfil from './pages/Perfil';
-import ReceitasFeitas from './pages/ReceitasFeitas';
-import ReceitasFavoritas from './pages/ReceitasFavoritas';
+import {
+  Login,
+  Comidas,
+  Bebidas,
+  DetalhesComida,
+  DetalhesBebida,
+  ComidaEmProcesso,
+  BebidaEmProgresso,
+  Explorar,
+  ExplorarComidas,
+  ExplorarBebidas,
+  ComidasIngredientes,
+  BebidasIngredientes,
+  ComidaOrigem,
+  Perfil,
+  ReceitasFeitas,
+  ReceitasFavoritas,
+} from './pages';
 
 function App() {
   return (
@@ -43,7 +45,7 @@ function App() {
         path="/explorar/bebidas/ingredientes"
         component={ BebidasIngredientes }
       />
-      <Route exact path="/explorar/comidas/area" component={ ComidasOrigem } />
+      <Route exact path="/explorar/comidas/area" component={ ComidaOrigem } />
       <Route exact path="/comidas/:id/in-progress" component={ ComidaEmProcesso } />
       <Route exact path="/bebidas/:id/in-progress" component={ BebidaEmProgresso } />
     </Switch>
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+export { default as BebidaEmProgresso } from './BebidaEmProgresso';
+export { default as Bebidas } from './Bebidas';
+export { default as BebidasIngredientes } from './BebidasIngredientes';
+export { default as ComidaEmProcesso } from './ComidaEmProcesso';
+export { default as ComidaOrigem } from './ComidaOrigem';
+export { default as Comidas } from './Comidas';
+export { default as ComidasIngredientes } from './ComidasIngredientes';
+export { default as DetalhesBebida } from './DetalhesBebida';
+export { default as DetalhesComida } from './DetalhesComida';
+export { default as Explorar } from './Explorar';
+export { default as ExplorarBebidas } from './ExplorarBebidas';
+export { default as ExplorarComidas } from './ExplorarComidas';
+export { default as Login } from './Login';
+export { default as Perfil } from './Perfil';
+export { default as ReceitasFavoritas } from './ReceitasFavoritas';
+export { default as ReceitasFeitas } from './ReceitasFeitas';
