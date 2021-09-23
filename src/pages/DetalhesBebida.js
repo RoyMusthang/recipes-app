95329399723b882632b36277d90f7b17a10151c4
rjanovicci <rjanovicci@hotmail.com>
2021-09-23 17:03:05 -0400

    ate 37

--- src/pages/DetalhesBebida.js
+++ src/pages/DetalhesBebida.js
@@ -12,25 +12,26 @@ function DetalhesBebida() {
   useEffect(() => {
     const fetchApi = async () => {
       const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRequest}`;
-      const urlRandom = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+      const urlRandom = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
       const response = await fetch(url);
       const results = await response.json();
       const randomResponse1 = await fetch(urlRandom);
       const randomResults1 = await randomResponse1.json();
-      const randomResponse2 = await fetch(urlRandom);
-      const randomResults2 = await randomResponse2.json();
-      setRandoms([randomResults1.drinks[0], randomResults2.drinks[0]]);
+      setRandoms(randomResults1.meals);
       setDrinkDetail(results.drinks);
     };
     fetchApi();
   }, [idRequest]);
 
   const ingredients = [];
+  const limitCards = 6;
 
   if (drinkDetail.length !== 0) {
     for (let i = 1; i <= Number('15'); i += 1) {
       if (drinkDetail[0][`strIngredient${i}`].length !== 0) {
-        ingredients.push(drinkDetail[0][`strIngredient${i}`]);
+        const ing = `${drinkDetail[0][`strIngredient${i}`]}`;
+        const mes = `${drinkDetail[0][`strMeasure${i}`]}`;
+        ingredients.push(`${ing} ${(!mes) ? '' : mes}`);
       } else break;
     }
   }
@@ -58,8 +59,7 @@ function DetalhesBebida() {
           >
             Favoritar
           </button>
-          <h4 data-testid="recipe-category">{ drinkDetail[0].strCategory }</h4>
-
+          <h4 data-testid="recipe-category">{ drinkDetail[0].strAlcoholic }</h4>
           <ul>
             { ingredients.map((ingredient, i) => (
               <li
@@ -71,15 +71,14 @@ function DetalhesBebida() {
             )) }
           </ul>
           <p data-testid="instructions">{ drinkDetail[0].strInstructions }</p>
-          <iframe title="Video" data-testid="video" src={ drinkDetail[0].strYoutube } />
-          {
-            randoms.map((item, i) => (
+          <div className="recomendation-container">
+            {randoms.filter((_, i2) => (i2 < limitCards)).map((item, i) => (
               <div key={ `${i}-${item}` } data-testid={ `${i}-recomendation-card` }>
-                <img src={ item.strDrinkThumb } alt="Bebida Recomendada" width="250px" />
-                <h2>{ item.strDrink }</h2>
+                <h4 data-testid={ `${i}-recomendation-title` }>{ item.strMeal }</h4>
+                <img src={ item.strMealThumb } alt="Bebida Recomendada" width="150px" />
               </div>
-            ))
-          }
+            ))}
+          </div>
           <button
             type="button"
             data-testid="start-recipe-btn"
--- src/pages/DetalhesComida.js
+++ src/pages/DetalhesComida.js
@@ -12,25 +12,26 @@ function DetalhesComida() {
   useEffect(() => {
     const fetchApi = async () => {
       const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idRequest}`;
-      const urlRandom = 'https://www.themealdb.com/api/json/v1/1/random.php';
+      const urlRandom = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
       const response = await fetch(url);
       const results = await response.json();
       const randomResponse1 = await fetch(urlRandom);
       const randomResults1 = await randomResponse1.json();
-      const randomResponse2 = await fetch(urlRandom);
-      const randomResults2 = await randomResponse2.json();
-      setRandoms([randomResults1.meals[0], randomResults2.meals[0]]);
+      setRandoms(randomResults1.drinks);
       setMealDetail(results.meals);
     };
     fetchApi();
   }, [idRequest]);
 
   const ingredients = [];
+  const limitCards = 6;
 
   if (mealDetail.length !== 0) {
     for (let i = 1; i <= Number('20'); i += 1) {
       if (mealDetail[0][`strIngredient${i}`].length !== 0) {
-        ingredients.push(mealDetail[0][`strIngredient${i}`]);
+        const ing = `${mealDetail[0][`strIngredient${i}`]}`;
+        const mes = `${mealDetail[0][`strMeasure${i}`]}`;
+        ingredients.push(`${ing} ${(!mes) ? '' : mes}`);
       } else break;
     }
   }
@@ -59,7 +60,6 @@ function DetalhesComida() {
             Favoritar
           </button>
           <h4 data-testid="recipe-category">{ mealDetail[0].strCategory }</h4>
-
           <ul>
             { ingredients.map((ingredient, i) => (
               <li
@@ -72,14 +72,14 @@ function DetalhesComida() {
           </ul>
           <p data-testid="instructions">{ mealDetail[0].strInstructions }</p>
           <iframe title="Video" data-testid="video" src={ mealDetail[0].strYoutube } />
-          {
-            randoms.map((item, i) => (
+          <div className="recomendation-container">
+            {randoms.filter((_, i2) => (i2 < limitCards)).map((item, i) => (
               <div key={ `${i}-${item}` } data-testid={ `${i}-recomendation-card` }>
-                <img src={ item.strMealThumb } alt="Comida Recomendada" width="250px" />
-                <h2>{ item.strMeal }</h2>
+                <h4 data-testid={ `${i}-recomendation-title` }>{ item.strDrink }</h4>
+                <img src={ item.strDrinkThumb } alt="Bebida Recomendada" width="150px" />
               </div>
-            ))
-          }
+            ))}
+          </div>
           <button
             type="button"
             data-testid="start-recipe-btn"
