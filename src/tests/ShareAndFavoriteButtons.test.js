effd5a43da8b8435c945dc542208456169dfee87
Bruno Pinho <brunopinho@outlook.com>
2021-09-24 17:44:13 -0300

    iniciado testes

---
+++ src/tests/ShareAndFavoriteButtons.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+
+describe('Botões de favoritar e compartilhar', () => {
+  const favBtnTestId = '';
+
+  describe('Testa o botão de favoritar', () => {
+
+  });
+});
