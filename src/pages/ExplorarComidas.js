90fc4e4765405c8a60f9a448bfc4f7ffd8da6d79
Murilo Rainho <murilorainho01@gmail.com>
2021-09-28 17:51:41 -0300

    requisitos 70-74 finalizados

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 // import PropTypes from 'prop-types';
@@ -7,6 +8,21 @@ function ExplorarComidas() {
   return (
     <div>
       <Header title="Explorar Comidas" renderSearchButton={ false } />
+      <section>
+        <Link
+          data-testid="explore-by-ingredient"
+          to="/explorar/comidas/ingredientes"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          data-testid="explore-by-area"
+          to="/explorar/comidas/area"
+        >
+          Por Local de Origem
+        </Link>
+        <Link data-testid="explore-surprise" to="/comidas/52771">Me Surpreenda!</Link>
+      </section>
       <Footer />
     </div>
   );
